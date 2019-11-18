2720efd35078cb026e0087d9d9be37ebb6e664a0
xistian7 <cris.batea@gmail.com>
2019-11-18 19:50:58 +0100

    base

---
+++ product/inventory/tpl/inventory.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+// Protection to avoid direct call of template
+if (empty($conf) || ! is_object($conf))
+{
+	print "Error, template page can't be called as URL";
+	exit;
+}
+
+?>
+
+
+TODO...
+
